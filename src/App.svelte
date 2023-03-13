ac5f593f86b18613213183a3d3cc7eb2b8df990b
Henrik Giesel <henrikgiesel@Wolnosc.fritz.box>
2023-03-13 20:09:41 +0100

    Add description

--- src/App.svelte
+++ src/App.svelte
@@ -27,11 +27,15 @@
 
 <List list="{list}" let:entry>
   {#if entry.item.type === 'folder'}
+    <!-- This on:click errors: Argument of type 'Inode' is not assignable to parameter of type 'Folder' -->
     <button on:click="{() => folderClicked(entry.item)}">
+      <!-- This works fine -->
       {entry.item.name}: {entry.item.entries.length}
     </button>
   {:else}
+    <!-- Same here: This on:click errors: Argument of type 'Inode' is not assignable to parameter of type 'File' -->
     <button on:click="{() => fileClicked(entry.item)}">
+      <!-- This works fine -->
       {entry.item.name}.{entry.item.extension}
     </button>
   {/if}
